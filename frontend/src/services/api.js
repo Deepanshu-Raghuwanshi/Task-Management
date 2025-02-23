0c72c407de5634b91fb672381727dcd876b55270
Deepanshu-Raghuwanshi <dipanshuraghuwanshio@gmail.com>
2025-02-23 13:25:13 +0530

    change for vite base url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.BE_BASE_URL || "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_BE_BASE_URL || "http://localhost:5000/api",
 });
 
 API.interceptors.request.use((config) => {
